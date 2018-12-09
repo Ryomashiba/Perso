264d4f1c190008dcefa49a9665b4146cb170a7ba
GIBERT Clément <ryomashiba@hotmail.fr>
2018-12-09 15:43:47 +0100

    ajout du parametre dans l'URL

--- HelloWord/HelloWord/Controllers/HomeController.vb
+++ HelloWord/HelloWord/Controllers/HomeController.vb
@@ -2,13 +2,10 @@
 
 Namespace Controllers
     Public Class HomeController
-
         Inherits Controller
-
         ' GET: Home
-        Function Index()
-            Return "hello World MVC"
+        Function Index(id As String)
+            Return "hello " + id 'affiche hello + le paramettre indiquer de l'URL si celui ci est un string
         End Function
     End Class
-
 End Namespace
