72a81e3c4825de05c8b035baee71e81755af4ce2
GIBERT Clément <ryomashiba@hotmail.fr>
2018-12-09 14:07:12 +0100

    Test
    test de commit

---
+++ HelloWord/HelloWord/Controllers/HomeController.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HomeController
+
+        Inherits Controller
+
+        ' GET: Home
+        Function Index()
+            Return "hello World MVC"
+        End Function
+    End Class
+
+End Namespace
